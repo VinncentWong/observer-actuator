805ec4ba3d90003b9e18a134033eeb5f4db4e906
CentWong <vinncentwong1@gmail.com>
2024-07-01 16:00:35 +0700

    initial commit

---
+++ src/main/java/centwong/actuator/medium/actuator_medium/ActuatorMediumApplication.java
@@ -0,0 +1,13 @@
+package centwong.actuator.medium.actuator_medium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorMediumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorMediumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/centwong/actuator/medium/actuator_medium/ActuatorMediumApplicationTests.java
@@ -0,0 +1,13 @@
+package centwong.actuator.medium.actuator_medium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorMediumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
